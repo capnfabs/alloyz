434610da4aea4531199901de8783e18f86cdb1a7
Fabian Tamp <fabian.tamp@gmail.com>
2025-06-22 07:37:34 +0200

    Fix service worker so that it serves from the cache only if the fetch fails

--- service-worker.js
+++ service-worker.js
@@ -19,8 +19,15 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        // update the cache before serving
+        const responseClone = response.clone();
+        caches.open('v1').then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
